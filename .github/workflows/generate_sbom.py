5340cfc9a5a6b698d541c967a4d2fc4b6526a3c5
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-03-21 18:09:11 -0600

    Add SBOM

---
+++ .github/workflows/generate_sbom.py
@@ -0,0 +1,19 @@
+from requests import get
+from json import dumps
+
+with open("opentelemetry-python.spdx.json", "w") as sbom_file:
+    sbom_file.write(
+        dumps(
+            get(
+                (
+                    "https://api.github.com/repos/open-telemetry/"
+                    "opentelemetry-python/dependency-graph/sbom"
+                ),
+                headers={
+                    "Accept": "application/vnd.github+json",
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+            ).json(),
+            indent=4
+        )
+    )
